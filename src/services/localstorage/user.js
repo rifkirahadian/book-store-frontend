41300d28747ebc81c798b61ed5650519d15fcfff
Rifki Rahadian <rrahadian94@gmail.com>
2024-03-30 10:49:41 +0700

    fix: localstorage issue

--- src/services/localstorage/user.js
+++ src/services/localstorage/user.js
@@ -1,16 +1,23 @@
 export const getUser = () => {
-  const user = localStorage.getItem('user');
-  if (user) {
-    return JSON.parse(user);
+  if (typeof window !== 'undefined') {
+    const user = localStorage.getItem('user');
+    if (user) {
+      return JSON.parse(user);
+    }
   }
+  
 
   return null;
 }
 
 export const setUser = (payload) => {
-  localStorage.setItem('user',JSON.stringify(payload));
+  if (typeof window !== 'undefined') {
+    localStorage.setItem('user',JSON.stringify(payload));
+  }
 };
 
 export const logout = () => {
-  localStorage.removeItem('user');
+  if (typeof window !== 'undefined') {
+    localStorage.removeItem('user');
+  }
 };
